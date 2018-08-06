bcc1d7fa55db3650fbf34926ca26158a800d8069
xiaopenggeisnice <42140536+xiaopenggeisnice@users.noreply.github.com>
2018-08-06 21:23:08 +0800

    666尼古拉斯凯鹏
    666尼古拉斯凯鹏

--- nongmin.java
+++ nongmin.java
@@ -1,2 +1,11 @@
 锄禾日当午
 汗滴禾下土
+没错，我就是万众瞩目
+世界上最帅的男人
+尼古拉斯凯鹏
+yeah
+kiki do you love me 
+are you riding
+say you never ever leave from beside me
+cause i want ya and i need ya
+and i am down for you always
